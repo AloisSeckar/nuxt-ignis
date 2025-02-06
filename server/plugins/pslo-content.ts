ea37c7c64f716410c0102f75a7c1ca9aac4cabc1
Alois Sečkár <ellrohir@seznam.cz>
2025-02-06 21:54:47 +0100

    chore: remove comment for fixed issue

--- server/plugins/pslo-content.ts
+++ server/plugins/pslo-content.ts
@@ -7,8 +7,6 @@ export default defineNitroPlugin((nitroApp) => {
   const config = useRuntimeConfig().public.ignis
   if (config.content && config.pslo.enabled && config.pslo.content) {
     console.log('elrh-pslo plugin initialized')
-    // @ts-expect-error typecheck for the hook
-    // TODO fix this - the example comes from https://content.nuxt.com/recipes/hooks, so it should work
     nitroApp.hooks.hook('content:file:beforeParse', (file) => {
       console.log('Treating Markdown with elrh-pslo')
       if (file._id.endsWith('.md')) {
