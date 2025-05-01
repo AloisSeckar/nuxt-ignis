c1f8fc57c175e3b58d20ec79498a5aa8b0e21453
Alois Sečkár <ellrohir@seznam.cz>
2025-05-01 23:07:54 +0200

    fix: 03 demo needs extra vueform config file

---
+++ demos/03_full/vueform.config.ts
@@ -0,0 +1,18 @@
+// TODO how to auto-import this from the base layer?
+
+import en from '@vueform/vueform/locales/en'
+import vueform from '@vueform/vueform/dist/vueform'
+import { defineConfig } from '@vueform/vueform'
+
+// add Vueform type definitions
+import '@vueform/vueform/types/index.d.ts'
+
+// add default CSS styles
+// can be placed anywhere else in your project
+import '@vueform/vueform/dist/vueform.css'
+
+export default defineConfig({
+  theme: vueform,
+  locales: { en },
+  locale: 'en',
+})
