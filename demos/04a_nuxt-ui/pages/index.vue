3d4a668c062e211388326f745e2c3451bca00eaa
Alois Sečkár <ellrohir@seznam.cz>
2025-06-02 22:20:45 +0200

    test: demo apps for testing UI features

---
+++ demos/04a_nuxt-ui/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Ignis Nuxt UI demo</h1>
+        <UButton>Test Button</UButton>
+        <ULink to="/">ULink</ULink>
+    </div>
+</template>
---
+++ demos/04b_tailwind/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="h-screen bg-slate-800 text-amber-300 flex items-center justify-center">
+        <h1 class="text-xl font-bold hover:text-orange-200">Ignis Tailwind Demo</h1>
+    </div>
+</template>
