8be90880534f9f08b39d5359ea86f1f95c6c4af6
Alois Sečkár <ellrohir@seznam.cz>
2025-06-07 18:20:03 +0200

    build: bump default compatibility date

--- core/nuxt.config.ts
+++ core/nuxt.config.ts
@@ -12,7 +12,7 @@ const nuxtConfig = defu(ignisFeatures, {
   ],
 
   // https://nuxt.com/docs/api/nuxt-config#compatibilitydate
-  compatibilityDate: '2025-05-21',
+  compatibilityDate: '2025-06-07',
 
   // simple eslint config - see eslint.config.mjs
   eslint: {
