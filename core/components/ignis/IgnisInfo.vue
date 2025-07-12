10f308843ab1411c4b89b64b22cfbf44def40866
Alois Sečkár <ellrohir@seznam.cz>
2025-07-12 19:53:43 +0200

    refactor: remove "/second" page from core (belongs to /demo folder)

--- core/components/ignis/IgnisInfo.vue
+++ core/components/ignis/IgnisInfo.vue
@@ -2,25 +2,7 @@
   <div>
     <IgnisHeader />
     <IgnisFeatureList />
-
-    <div v-if="pages" class="link">
-      <NuxtLink to="/_ignis-two">
-        {{ useIgnisT("goto2") }}
-      </NuxtLink>
-    </div>
-
     <CurrentTime />
     <IgnisFooter />
   </div>
 </template>
-
-<script setup lang="ts">
-const pages = useRuntimeConfig().public.ignis.pages
-</script>
-
-<style scoped lang="css">
-.link {
-  text-align: center;
-  margin-bottom: 4px;
-}
-</style>
