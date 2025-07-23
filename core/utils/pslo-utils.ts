9e78b463ea95e8db17cd2e902c73a7891888f20b
Alois Sečkár <ellrohir@seznam.cz>
2025-07-23 22:48:30 +0200

    fix: actually fix: pslo integration with Nuxt Content (#103)

--- core/utils/pslo-utils.ts
+++ core/utils/pslo-utils.ts
@@ -1,10 +1,11 @@
 import { preventSingleLetterOrphans } from 'elrh-pslo'
+import { log } from './consola'
 
 // this util function proxies "preventSingleLetterOrphans" function
 // it only applies the text transformation if relevant options is set up
 export function pslo(text: string) {
   if (process.env.NUXT_PUBLIC_IGNIS_PSLO_ENABLED === 'true') {
-    log.warn(`elrh-pslo activated`)
+    log.debug(`treating text input with elrh-pslo`)
     return preventSingleLetterOrphans(text)
   } else {
     return text
