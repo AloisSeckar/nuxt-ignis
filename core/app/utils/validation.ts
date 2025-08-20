42a863002d46484ba92276a809b773e7c2fc53d7
AloisSeckar <ellrohir@seznam.cz>
2025-08-20 12:18:31 +0200

    fix: param order in validator functions (#104)

--- core/app/utils/validation.ts
+++ core/app/utils/validation.ts
@@ -2,7 +2,7 @@ import type { ObjectEntries, ObjectSchema } from 'valibot'
 import type { ZodObject } from 'zod/v4'
 
 // quick object schema validation - zod variant
-export async function isValidByZod(obj: unknown, schema: ZodObject): Promise<boolean> {
+export async function isValidByZod(schema: ZodObject, obj: unknown): Promise<boolean> {
   const config = useRuntimeConfig().public.ignis
   if (config.preset.validation === 'zod' || config.zod === true) {
     return schema.safeParse(obj).success
@@ -13,7 +13,7 @@ export async function isValidByZod(obj: unknown, schema: ZodObject): Promise<boo
 }
 
 // quick object schema validation - valibot variant
-export async function isValidByValibot(obj: unknown, schema: ObjectSchema<ObjectEntries, undefined>): Promise<boolean> {
+export async function isValidByValibot(schema: ObjectSchema<ObjectEntries, undefined>, obj: unknown): Promise<boolean> {
   const config = useRuntimeConfig().public.ignis
   if (config.preset.validation === 'valibot' || config.valibot === true) {
     const v = (await useValibot())!
