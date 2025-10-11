b28814e86b1c932b698cb51be8e0ac8e1bcf0193
Alois Sečkár <ellrohir@seznam.cz>
2025-10-11 09:29:28 +0200

    feat: enhance CLI setup script

--- core/bin/setup.js
+++ core/bin/setup.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
-import { createFileFromWebTemplate, promptUser, showMessage, updateConfigFile, updateJsonFile, updateTextFile } from 'elrh-cosca'
+import { 
+  createFileFromWebTemplate, deletePath, getPackageManager, hasJsonKey,
+  pathExists, promptUser, removeFromJsonFile, showMessage,
+  updateConfigFile, updateJsonFile, updateTextFile,
+} from 'elrh-cosca'
 
 /**
  * CLI tool to setup Nuxt Ignis for a new project.
@@ -10,6 +14,14 @@ import { createFileFromWebTemplate, promptUser, showMessage, updateConfigFile, u
  * Has two modes:
  * - automatic - no prompts except the first one
  * - interactive - with prompts for each step
+ * 
+ * The script will:
+ *  1) add `nuxt-ignis` into `package.json` dependencies, remove `nuxt`, `vue` and `vue-router` if present and adjust `pnpm` settings if `pnpm` is used
+ *  2) add `extends: ['nuxt-ignis']` to `nuxt.config.ts`
+ *  3) create/update `.npmrc` file
+ *  4) update `.gitignore` file
+ *  5) create default `vitest.config.ts` file and add test-related scripts into `package.json`
+ *  6) clear node_modules and lock file(s)
  */
 async function nuxtIgnisSetup() {
   showMessage('NUXT IGNIS SETUP')
@@ -27,30 +39,81 @@ async function nuxtIgnisSetup() {
     console.error('Error adding \'nuxt-ignis\' dependency:\n', error.message)
   }
 
-  // 1.2 - adjust pnpm settings
-  const pnpmSettings = auto || await promptUser('This will adjust pnpm settings in your \'package.json\'. Proceed?')
-  if (pnpmSettings) {
-    try {
-      await updateJsonFile('package.json', 'pnpm', {
-        onlyBuiltDependencies: [
-          '@parcel/watcher',
-          '@tailwindcss/oxide',
-          'better-sqlite3',
-          'esbuild',
-          'maplibre-gl',
-          'puppeteer',
-          'sharp',
-          'unrs-resolver',
-          'vue-demi',
-        ],
-      }, true)
-    } catch (error) {
-      console.error('Error adjusting pnpm settings:\n', error.message)
+  // 1.2 - remove now obsolete nuxt, vue and vue-router
+  const removeDeps = isAutoRun || await promptUser('As \'nuxt-ignis\' provides \'nuxt\', \'vue\' and \'vue-router\' dependencies out of the box, do you want to remove them from your \'package.json\' to avoid duplications and possible version clashes?')
+  if (removeDeps) {
+    if (hasJsonKey('package.json', 'dependencies.nuxt')) {
+      try {
+        await removeFromJsonFile('package.json', 'dependencies.nuxt', true)
+      } catch (error) {
+        console.error('Error removing \'nuxt\' dependency:\n', error.message)
+      }
     }
-    try {
-      await updateJsonFile('package.json', 'packageManager', 'pnpm@10.17.0', true)
-    } catch (error) {
-      console.error('Error setting packageManager:\n', error.message)
+    if (hasJsonKey('package.json', 'dependencies.vue')) {
+      try {
+        await removeFromJsonFile('package.json', 'dependencies.vue', true)
+      } catch (error) {
+        console.error('Error removing \'vue\' dependency:\n', error.message)
+      }
+    }
+    if (hasJsonKey('package.json', 'dependencies.vue-router')) {
+      try {
+        await removeFromJsonFile('package.json', 'dependencies.vue-router', true)
+      } catch (error) {
+        console.error('Error removing \'vue-router\' dependency:\n', error.message)
+      }
+    }
+    if (hasJsonKey('package.json', 'devDependencies.nuxt')) {
+      try {
+        await removeFromJsonFile('package.json', 'devDependencies.nuxt', true)
+      } catch (error) {
+        console.error('Error removing \'nuxt\' devDependency:\n', error.message)
+      }
+    }
+    if (hasJsonKey('package.json', 'devDependencies.vue')) {
+      try {
+        await removeFromJsonFile('package.json', 'devDependencies.vue', true)
+      } catch (error) {
+        console.error('Error removing \'vue\' devDependency:\n', error.message)
+      }
+    }
+    if (hasJsonKey('package.json', 'devDependencies.vue-router')) {
+      try {
+        await removeFromJsonFile('package.json', 'devDependencies.vue-router', true)
+      } catch (error) {
+        console.error('Error removing \'vue-router\' devDependency:\n', error.message)
+      }
+    }
+  }
+
+  // 1.3 - adjust pnpm settings
+  if (getPackageManager() === 'pnpm') {
+    const pnpmSettings = auto || await promptUser('This will adjust pnpm settings in your \'package.json\'. Proceed?')
+    if (pnpmSettings) {
+      try {
+        // allow related build scripts
+        await updateJsonFile('package.json', 'pnpm', {
+          onlyBuiltDependencies: [
+            '@parcel/watcher',
+            '@tailwindcss/oxide',
+            'better-sqlite3',
+            'esbuild',
+            'maplibre-gl',
+            'puppeteer',
+            'sharp',
+            'unrs-resolver',
+            'vue-demi',
+          ],
+        }, true)
+      } catch (error) {
+        console.error('Error adjusting pnpm settings:\n', error.message)
+      }
+      // set pnpm as package manager
+      try {
+        await updateJsonFile('package.json', 'packageManager', 'pnpm@10.18.0', true)
+      } catch (error) {
+        console.error('Error setting packageManager:\n', error.message)
+      }
     }
   }
 
@@ -67,10 +130,14 @@ async function nuxtIgnisSetup() {
 
   // 3 - .npmrc file
   try {
-    await createFileFromWebTemplate('https://raw.githubusercontent.com/AloisSeckar/nuxt-ignis/refs/heads/main/core/.npmrc',
-      '.npmrc', auto, 'This will set \'.npmrc\' file for your project. Continue?')
+    if (pathExists('.npmrc')) {
+      await updateTextFile('.npmrc', ['shamefully-hoist=true'], isAutoRun, 'This will adjust \'.npmrc\' file in your project. Continue?')
+    } else {
+      await createFileFromWebTemplate('https://raw.githubusercontent.com/AloisSeckar/nuxt-ignis/refs/heads/main/core/.npmrc',
+        '.npmrc', isAutoRun, 'This will add \'.npmrc\' file for your project. Continue?')
+    }
   } catch (error) {
-    console.error('Error setting .npmrc file:\n', error.message)
+    console.error('Error setting \'.npmrc\':\n', error.message)
   }
 
   // 4 - .gitignore file
@@ -84,6 +151,81 @@ async function nuxtIgnisSetup() {
   } catch (error) {
     console.error('Error updating .gitignore file:\n', error.message)
   }
+
+  // 5) nuxt-spec related setup
+  const setupNuxtSpec = auto || await promptUser('Nuxt Ignis comes with support for testing. Do you want to set up the default test settings now?')
+  if (setupNuxtSpec) {
+    // create vitest.config.ts
+    try {
+      await createFileFromWebTemplate('https://raw.githubusercontent.com/AloisSeckar/nuxt-spec/refs/heads/main/config/vitest.config.ts.template',
+        'vitest.config.ts', true, 'This will create a new \'vitest.config.ts\' file for your project. Continue?')
+    } catch (error) {
+      console.error('Error setting up \'vitest.config.ts\':\n', error.message)
+    }
+
+    // add scripts for running tests into package.json
+    try {
+      await updateJsonFile('package.json', 'scripts', {
+        'test': 'vitest run',
+        'test-u': 'vitest run -u',
+        'test-i': 'vitest',
+      }, true, 'This will adjust the test-related commands in your \'package.json\'. Continue?')
+    } catch (error) {
+      console.error('Error adjusting scripts in \'package.json\':\n', error.message)
+    }
+  }
+
+  // 6) clear node_modules and lock file(s)
+  const prepareForReinstall = auto || await promptUser('Dependencies should be re-installed now. Do you want to remove node_modules and the lock file?')
+  if (prepareForReinstall) {
+    if (pathExists('node_modules')) {
+      try {
+        await deletePath('node_modules', true)
+      } catch (error) {
+        console.error('Error deleting \'node_modules\':\n', error.message)
+      }
+    }
+    if (pathExists('package-lock.json')) {
+      try {
+        await deletePath('package-lock.json', true)
+      } catch (error) {
+        console.error('Error deleting \'package-lock.json\':\n', error.message)
+      }
+    }
+    if (pathExists('pnpm-lock.yaml')) {
+      try {
+        await deletePath('pnpm-lock.yaml', true)
+      } catch (error) {
+        console.error('Error deleting \'pnpm-lock.yaml\':\n', error.message)
+      }
+    }
+    if (pathExists('yarn.lock')) {
+      try {
+        await deletePath('yarn.lock', true)
+      } catch (error) {
+        console.error('Error deleting \'yarn.lock\':\n', error.message)
+      }
+    }
+    if (pathExists('bun.lockb')) {
+      try {
+        await deletePath('bun.lockb', true)
+      } catch (error) {
+        console.error('Error deleting \'bun.lockb\':\n', error.message)
+      }
+    }
+    if (pathExists('deno.lock')) {
+      try {
+        await deletePath('deno.lock', true)
+      } catch (error) {
+        console.error('Error deleting \'deno.lock\':\n', error.message)
+      }
+    }
+  }
+
+  // 7) inform user
+  showMessage('')
+  showMessage('NUXT IGNIS SETUP COMPLETE', 2)
+  showMessage(`Proceed with \`${getPackageManager()} install\` to get started.`)
 }
 
 nuxtIgnisSetup().catch((err) => {
