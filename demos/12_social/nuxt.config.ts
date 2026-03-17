d81a72bdcd40fe0593760832a25de7243633ff96
Alois Sečkár <ellrohir@seznam.cz>
2026-03-17 22:15:00 +0100

    docs: note down the option resolving behavior

--- demos/12_social/nuxt.config.ts
+++ demos/12_social/nuxt.config.ts
@@ -4,19 +4,19 @@ export default defineNuxtConfig({
     // while having 'nuxt-ignis' as dependency in your package.json
     '../../core',
   ],
+
   /*
-  ignis: {
+  ignisContent: {
     social: {
+      // TODO this is currently not enough to enable the module, .env variable is required for this
       enabled: true,
-      // TODO currently this option is not being picked up
+      // however, once enabled, this will have precedence over .env variable
       url: 'https://nuxt-migni.com',
     },
   },
-  */
-  /*
   socialShare: {
-    // this will get precedence over ignis env variable
-    // baseUrl: 'https://nuxt-ignis.com',
+    // this has the highest priority and will overwrite Nuxt Ignis config / .env variable
+    baseUrl: 'https://nuxt-ignis.com',
   },
   */
 })
