2f2bdf751f376fdfa7f66a338d1bfbdaee304470
Alois Sečkár <ellrohir@seznam.cz>
2026-04-03 11:30:14 +0200

    test: update module isolated e2e tests

--- modules/03-db/playground/app.vue
+++ modules/03-db/playground/app.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>
-    Nuxt module playground!
-  </div>
+  <header>
+    <h1>@nuxt-ignis/db</h1>
+    <p>Module playground</p>
+  </header>
+  <main>
+    <section>
+      <h2>About</h2>
+      <p>Service module providing DB features for <a href="https://nuxt-ignis.com/">Nuxt Ignis</a></p>
+    </section>
+  </main>
 </template>
 
-<script setup>
+<script setup lang="ts">
 </script>
