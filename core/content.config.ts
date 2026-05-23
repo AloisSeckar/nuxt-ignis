b702bcf7de47121c44c6cc19296888f3656d4fdf
Alois Sečkár <ellrohir@seznam.cz>
2026-05-23 23:13:14 +0200

    refactor: optimize the future bundle size

--- core/content.config.ts
+++
@@ -1,8 +0,0 @@
-// minimal config for Nuxt Content
-// currently required to be loaded like this...
-
-import { loadContentConfig } from '@nuxt-ignis/content/config'
-
-export default loadContentConfig({
-  // custom config here
-})
